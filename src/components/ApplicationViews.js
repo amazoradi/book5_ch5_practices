b0ef21479afd69d7f7c550cece2ee00b1cce3a97
amazoradi <amazoradi@gmail.com>
2018-11-19 11:31:16 -0600

    ch4 practice of getting data from the API complete

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -6,56 +6,37 @@ import EmployeeList from './employee/EmployeeList'
 import OwnerList from "./owners/OwnerList"
 
 
-class ApplicationViews extends Component {
-  employeesFromAPI = [
-    { id: 1, name: "Jessica Younker" },
-    { id: 2, name: "Jordan Nelson" },
-    { id: 3, name: "Zoe LeBlanc" },
-    { id: 4, name: "Blaise Roberts" }
-  ]
-
-  locationsFromAPI = [
-    { id: 1, name: "Nashville North", address: "500 Circle Way" },
-    { id: 2, name: "Nashville South", address: "10101 Binary Court" },
-    { id: 3, name: "Nashville East", address: "123 Fictitious Ave N" },
-    { id: 4, name: "Nashville West", address: "999 Imagiation Station S" }
-  ]
-
-
-  animalsFromAPI = [
-    { id: 1, name: "Luna", type: "cat" },
-    { id: 2, name: "Chewy", type: "cat" },
-    { id: 3, name: "Beorn", type: "Doberman" },
-    { id: 4, name: "King", type: "German Shephard" },
-    { id: 5, name: "Santa's Little Helper", type: "Greyhound" }
-  ]
-
-  ownersFromAPI = [
-    { id: 1, name: "Ryan Tanay" , phone: 6151112233},
-    { id: 2, name: "Emma Beaton", phone: 8059690012 },
-    { id: 3, name: "Dani Adkins", phone: 3129876543},
-    { id: 4, name: "Adam Oswalt", phone: 6158743265 },
-    { id: 5, name: "Fletcher Bangs", phone: 6199680967},
-    { id: 6, name: "Angela Lee", phone: 8053343567}
-  ]
-
-  animalOwnersFromAPI = [
-    { id: 1, ownerId: 1, animalId: 4 },
-    { id: 2, ownerId: 2, animalId: 3 },
-    { id: 3, ownerId: 3, animalId: 2 },
-    { id: 4, ownerId: 4, animalId: 2 },
-    { id: 5, ownerId: 6, animalId: 4 },
-    { id: 6, ownerId: 5, animalId: 1 },
-    { id: 7, ownerId: 6, animalId: 2 }
-  ]
+export default class ApplicationViews extends Component {
 
   state = {
-    employees: this.employeesFromAPI,
-    locations: this.locationsFromAPI,
-    animals: this.animalsFromAPI,
-    owners: this.ownersFromAPI,
-    animalOwners: this.animalOwnersFromAPI
+    employees: [],
+    locations: [],
+    animals: [],
+    owners: [],
+    animalOwners: []
   }
+
+  componentDidMount() {
+    const newState = {}
+
+    fetch("http://localhost:5002/animals")
+      .then(response => response.json())
+      .then(animals => newState.animals = animals)
+      .then(() => fetch("http://localhost:5002/employees")
+        .then(response => response.json()))
+      .then(employees => newState.employees = employees)
+      .then(() => fetch("http://localhost:5002/locations")
+        .then(response => response.json()))
+      .then(locations => newState.locations = locations)
+      .then(() => fetch("http://localhost:5002/owners")
+        .then(response => response.json()))
+      .then(owners => newState.owners = owners) 
+      .then(() => fetch("http://localhost:5002/animalOwners")
+        .then(response => response.json()))
+      .then(animalOwners => newState.animalOwners = animalOwners)
+      .then(() => this.setState(newState))
+  }
+
 
   render() {
     return (
@@ -77,4 +58,3 @@ class ApplicationViews extends Component {
   }
 }
 
-export default ApplicationViews
